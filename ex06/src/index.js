2db1ac05243d57aefd94cb12435f2fbe339ee430
Bruno Lovric <brunoolovric@gmail.com>
2021-03-18 01:57:37 +0100

    added add object properties project

---
+++ ex06/src/index.js
@@ -0,0 +1,12 @@
+var lion = {
+    name: "Simba",
+    legs: 4,
+    tails: 1
+};
+function myFunction(roar, "roar-roar") {
+    lion[roar] = "roar-roar";
+    return lion;
+}
+myFunction(roar, "roar-roar");
+console.log(myFunction(roar, "roar-roar"));
+module.exports = myFunction;
