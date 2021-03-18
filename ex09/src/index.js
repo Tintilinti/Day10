a7a6e84ff53105c2ed0f84b3ebfb7d2d66d1e453
Bruno Lovric <brunoolovric@gmail.com>
2021-03-18 03:22:58 +0100

    added .hasOwnProperty() project

---
+++ ex09/src/index.js
@@ -0,0 +1,8 @@
+function myFunction(myObj, checkProp) {
+    if (myObj.hasOwnProperty(checkProp)) {
+        return myObj[checkProp];
+    } else {
+        return "Not found";
+    }
+}
+module.exports = myFunction;
